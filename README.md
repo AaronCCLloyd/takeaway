2bea56e839319e148f66fce5914796fc92c445d6
Aaron Lloyd <aaron.lloyd@aaron-codes.io>
2025-07-12 09:40:21 +0100

    Linking libtins to the project.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # takeaway
 Ordering TCP packets fast in real-time
+
+
+# Dependencies
+`pkg-config --cflags --libs libtins`
+should output
+`-I/usr/local/include/tins -L/usr/local/lib -ltins`
--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
 
-int main(int, char**){
+#include <tins/tins.h>
+
+using namespace Tins;
+
+int main(int, char **)
+{
     std::cout << "Hello, from takeaway!\n";
+
+    std::string dst_hw_addr = "00:11:22:33:44:55";
+    std::string src_hw_addr = "66:77:88:99:AA:BB";
+
+    EthernetII eth = EthernetII(dst_hw_addr, src_hw_addr) / IP() / TCP();
+
+    std::cout << "Destination MAC: " << eth.dst_addr() << std::endl;
+    std::cout << "Source MAC: " << eth.src_addr() << std::endl;
+    std::cout << "Payload Type: " << eth.payload_type() << std::endl;
 }
